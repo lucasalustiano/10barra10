79b24898999fdb8430fd05938e1c3de780a5f09b
lucasalustiano <lucas.salustiano@ccc.ufcg.edu.br>
2019-05-06 23:36:26 -0300

    ashduaishdi

--- R10-02-Rot-SetLinkedList-environment/src/main/java/adt/linkedList/set/SetLinkedListImpl.java
+++ R10-02-Rot-SetLinkedList-environment/src/main/java/adt/linkedList/set/SetLinkedListImpl.java
@@ -3,54 +3,57 @@ package adt.linkedList.set;
 import adt.linkedList.SingleLinkedListImpl;
 import adt.linkedList.SingleLinkedListNode;
 
-public class SetLinkedListImpl<T> extends SingleLinkedListImpl<T> implements SetLinkedList<T> {
+	public class SetLinkedListImpl<T> extends SingleLinkedListImpl<T> implements SetLinkedList<T> {
 
 	@Override
 	public void removeDuplicates() {
-		SingleLinkedListNode<T> aux1 = this.getHead();
-		SingleLinkedListNode<T> aux2 = this.getHead();
-		while (!aux1.isNIL()) {
-			while(!aux2.isNIL()) {
-				if (aux1.equals(aux2.getNext())) {
-					this.remove(aux2.getNext().getData());
-				}
-				aux2 = aux2.getNext();
-			}
-			aux1 = aux1.getNext();
-		}
-	}
+        SingleLinkedListNode<T> auxiliary1 = this.getHead();
+
+        while(!auxiliary1.getNext().isNIL()) {
+            SingleLinkedListNode<T> auxiliary2 = auxiliary1.getNext();
+            while(!auxiliary2.getNext().isNIL()) {
+                if(auxiliary1.getData() == auxiliary2.getData()) {
+                    auxiliary1 = auxiliary1.getNext();
+                    remove(auxiliary2.getData());
+                }
+                auxiliary2 = auxiliary2.getNext();
+            }
+            auxiliary1 = auxiliary1.getNext();
+        }
+
+    }
 
 	@Override
 	public SetLinkedList<T> union(SetLinkedList<T> otherSet) {
 		SetLinkedListImpl<T> result =  new SetLinkedListImpl<>();
-		SingleLinkedListNode<T> aux1 = this.getHead();
-		SingleLinkedListNode<T> aux2 = ((SingleLinkedListImpl<T>) otherSet).getHead();
+		SingleLinkedListNode<T> auxiliary1 = this.getHead();
+		SingleLinkedListNode<T> auxiliary2 = ((SingleLinkedListImpl<T>) otherSet).getHead();
 		
 		// Se o set atual for maior ou igual a other
 		if (this.size() >= otherSet.size()) {
-			while (!aux1.isNIL()) {
-				while(!aux2.isNIL()) {
-					if (aux1.equals(aux2.getNext())) {
-						result.insert(aux2.getData());
-						aux1 = aux1.getNext();
-						aux2 = aux2.getNext();
+			while (!auxiliary1.isNIL()) {
+				while(!auxiliary2.isNIL()) {
+					if (auxiliary1.equals(auxiliary2.getNext())) {
+						result.insert(auxiliary2.getData());
+						auxiliary1 = auxiliary1.getNext();
+						auxiliary2 = auxiliary2.getNext();
 					}
-					aux2 = aux2.getNext();
+					auxiliary2 = auxiliary2.getNext();
 				}
-				aux1 = aux1.getNext();
+				auxiliary1 = auxiliary1.getNext();
 			}
 		// Se other for maior que o set atual
 		} else {
-			while (!aux2.isNIL()) {
-				while(!aux1.isNIL()) {
-					if (aux2.equals(aux1.getNext())) {
-						result.insert(aux2.getData());
-						aux2 = aux2.getNext();
-						aux1 = aux1.getNext();
+			while (!auxiliary2.isNIL()) {
+				while(!auxiliary1.isNIL()) {
+					if (auxiliary2.equals(auxiliary1.getNext())) {
+						result.insert(auxiliary2.getData());
+						auxiliary2 = auxiliary2.getNext();
+						auxiliary1 = auxiliary1.getNext();
 					}
-					aux1 = aux1.getNext();
+					auxiliary1 = auxiliary1.getNext();
 				}
-				aux2 = aux2.getNext();
+				auxiliary2 = auxiliary2.getNext();
 			}
 		}
 		
@@ -61,14 +64,57 @@ public class SetLinkedListImpl<T> extends SingleLinkedListImpl<T> implements Set
 
 	@Override
 	public SetLinkedList<T> intersection(SetLinkedList<T> otherSet) {
-		//TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
+		SetLinkedListImpl<T> result =  new SetLinkedListImpl<>();
+		SingleLinkedListNode<T> auxiliary1 = this.getHead();
+		SingleLinkedListNode<T> auxiliary2 = ((SingleLinkedListImpl<T>) otherSet).getHead();
+		
+		// Se o set atual for maior ou igual a other
+		if (this.size() >= otherSet.size()) {
+			while (!auxiliary1.isNIL()) {
+				while(!auxiliary2.isNIL()) {
+					if (!auxiliary1.equals(auxiliary2.getNext()) && !auxiliary2.getNext().isNIL()) {
+						result.insert(auxiliary2.getData());
+						auxiliary1 = auxiliary1.getNext();
+						auxiliary2 = auxiliary2.getNext();
+					}
+					auxiliary2 = auxiliary2.getNext();
+				}
+				auxiliary1 = auxiliary1.getNext();
+			}
+		// Se other for maior que o set atual
+		} else {
+			while (!auxiliary2.isNIL()) {
+				while(!auxiliary1.isNIL()) {
+					if (!auxiliary2.equals(auxiliary1.getNext()) && !auxiliary1.getNext().isNIL()) {
+						result.insert(auxiliary2.getData());
+						auxiliary2 = auxiliary2.getNext();
+						auxiliary1 = auxiliary1.getNext();
+					}
+					auxiliary1 = auxiliary1.getNext();
+				}
+				auxiliary2 = auxiliary2.getNext();
+			}
+		}
+		
+		result.removeDuplicates();
+		
+		return result;
 	}
 
 	@Override
 	public void concatenate(SetLinkedList<T> otherSet) {
-		//TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
+		SetLinkedListImpl<T> result =  new SetLinkedListImpl<>();
+		SingleLinkedListNode<T> auxiliary1 = this.getHead();
+		SingleLinkedListNode<T> auxiliary2 = ((SingleLinkedListImpl<T>) otherSet).getHead();
+		
+		while (!auxiliary1.getNext().isNIL()) {
+			auxiliary1 = auxiliary1.getNext();
+		}
+		
+		auxiliary1.setNext(auxiliary2);
+		
+		result.removeDuplicates();	
+		
 	}
 
 }
